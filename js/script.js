455b5c65d459254a9ed9ab379633ecb30a76d954
Yeva <yevachichina@gmail.com>
2023-04-10 13:23:10 -0400

    lesson 2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,70 @@
-/* Cоздания приложения для учета просмотреных фильмов */
+'use strict';
 
-const numberOffilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+let NumberofFilms;
+
+function start () {
+	NumberofFilms = +prompt ('Сколько фильмов вы уже просмотрели?', '');
+
+	while (NumberofFilms == '' || NumberofFilms == null || isNaN(NumberofFilms)) {
+		NumberofFilms = +prompt ('Сколько фильмов вы уже просмотрели?', '');
+	}
+}
+
+start();
 
 const personalMovieDB = {
-	count:numberOffilms, 
-	movies:{},
-	actors:{},
-	genres:[],
+	count: NumberofFilms, 
+	movies: {}, 
+	actors: {}, 
+	genres: [], 
 	privat: false
 };
 
-const a = prompt('Один из посбедних просметреных фильмов',''), 
-	b = prompt('На сколько оцените его?',''),
-	c = prompt('Один из посбедних просметреных фильмов',''),
-	d = prompt('На сколько оцените его?','');
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++){
+		const a = prompt ('Один из послдкних просмотреных фильмов?',''),
+			b = prompt ('На сколько оцените его?','');
+		// number 2
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log('done');    
+		} else {
+			console.log('error');
+			i--;
+		}   
+	}
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+rememberMyFilms();
 
-console.log(personalMovieDB);
+function detectPersonalLevel () {
+	if (personalMovieDB.count < 10) {
+		console.log('Просмотрено довольна мало фильмов');
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+		console.log('Вы класичений зритель');
+	} else if (personalMovieDB.count >= 30) {
+		console.log('Вы киноман');
+	} else {
+		console.log('Произошла ошибка');
+	}
+}
+
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+	if (!hidden){
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+	for (let i = 1; i<= 3; i++){
+		personalMovieDB.genres[i - 1] = prompt (`Ваш любимый жанр под номером ${i}`, '');
+	}
+}
+
+writeYourGenres();
+// eslint-disable-next-line no-undef
+a = 1;
